61a940ab9f8e6cba63d8ba70d78c1c2e83a87a9d
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2024-06-24 20:34:30 +0700

    Add better messages for args in prune.py

--- acc-check.sh
+++ acc-check.sh
@@ -2,10 +2,10 @@
 
 
 models=(
-  "mobilenet_v3_small"
-  "mobilenet_v3_large"
-  "efficientnet_b1"
-  "efficientnet_b3"
+  # "mobilenet_v3_small"
+  # "mobilenet_v3_large"
+  # "efficientnet_b1"
+  # "efficientnet_b3"
   "densenet169"
   "densenet201"
   "vit_b_16"
@@ -15,15 +15,14 @@ dataset="Flowers102"
 
 for model in "${models[@]}"
 do
-  for i in {1..5}
-  do
+    sl="sparse_bsc"
+    path="models/pruned/0.8-RND-${model}-Flowers102-${sl}.pth"
     args=(
       --model $model
       --dataset $dataset
       --batch-size 8
-      --path models/${model}-${dataset}-${i}.pth
+      --path "${path}"
     )
 
     python3 -m benchmark.acc-check "${args[@]}"
-  done
 done
